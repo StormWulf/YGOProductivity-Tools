e2a73738ae55ff6a70a9de6501d84f526b9297c6
Jeff Falberg <auronzeroo@aim.com>
2017-02-19 21:31:26 -0500

    try-catch for card names that share a name with something else

--- ID conversion/ID conversion.py
+++ ID conversion/ID conversion.py
@@ -2,6 +2,7 @@
 # ID conversion for prerelease -> official cards
 import urllib.request as urr
 import urllib.error
+from urllib.parse import quote
 from urllib.error import HTTPError
 import sys
 import re
@@ -26,15 +27,14 @@ for line in listoflines :
     name = name.fetchone()
     name = str(name)
     name = name[2:-3]
-    name = name.replace(' ','_')
-    #name = str(name.encode('utf8'))
+    name = name.replace(' ','_').replace('#','').replace('-','-')
     #print(name)
-    wiki_url = "http://yugioh.wikia.com/wiki/" + name
+    wiki_url = "http://yugioh.wikia.com/wiki/" + quote(name)
     try:
         page = urr.urlopen(wiki_url)
         if page.getcode() == 200 :
             sourcepage = page.read()
-            source = sourcepage.decode("utf-8")    
+            source = sourcepage.decode("utf-8")   
             regexID = re.compile(r"(\d\d\d\d\d\d\d\d)<\/a><\/td><\/tr>")
             patternID = re.compile(regexID)
             try :
@@ -45,11 +45,35 @@ for line in listoflines :
                 query.write('update datas set id='+change_id+' where id='+line+';\n')
                 query.write('update texts set id='+change_id+' where id='+line+';\n')
             except IndexError :
-                pass
-                continue
+                wiki_url = "http://yugioh.wikia.com/wiki/" + quote(name) + "_(card)"
+                page = urr.urlopen(wiki_url)
+                if page.getcode() == 200 :
+                    sourcepage = page.read()
+                    source = sourcepage.decode("utf-8")   
+                    regexID = re.compile(r"(\d\d\d\d\d\d\d\d)<\/a><\/td><\/tr>")
+                    patternID = re.compile(regexID)
+                    try :
+                        change_id = re.findall(patternID, source)[0]
+                        #print(line,end="")
+                        line = line.rstrip()
+                        print(line+': '+change_id)
+                        query.write('update datas set id='+change_id+' where id='+line+';\n')
+                        query.write('update texts set id='+change_id+' where id='+line+';\n')
+                    except IndexError :
+                        print(name+" failed due to IndexError")
+                        pass
+                    continue
     except urllib.error.HTTPError as err :
         if err.code == 404 :
+           # try:
+               # wiki_url = "http://yugioh.wikia.com/wiki/" + line
+               # page = urr.urlopen(wiki_url)
+            print(name+" failed due to HTTPError")
+            #fail.write(name+" failed due to HTTPError\n")
             continue
+            #except urllib.error.HTTPError as err :
+                #pass
+            #continue
         else :
             raise                    
 query.close()
