7cdef593e3d4887af3e04b65d63543f9a5a390c9
Jeff Falberg <auronzeroo@aim.com>
2017-09-30 16:30:15 -0400

    Praise be to Lord RegEx

--- Wiki JS/Wiki_scraper.js
+++ Wiki JS/Wiki_scraper.js
@@ -2,26 +2,20 @@ var scraperjs = require('scraperjs');
 scraperjs.StaticScraper.create()
     .request({ url:'http://yugioh.wikia.com/wiki/Stardust_Dragon', encoding: "utf8"})
     .scrape(function($) {
+        setcode = []; 
+        $('dt').filter(function() {
+            return $(this).text().match(/Archetypes and series(.*)/);
+        }).nextUntil('dt').each(function(){
+                setcode.push($(this).text().trim());
+            });
         card = {};
-        $(".cardtablerowheader").map(function() {
+        $(".cardtablerowheader").each(function() {
             return $(this).each( function(index) {
                 card[$(this).text()] = $(this).next().text(); 
             });
         });
-        $(".cardtablespanrow").each( function(index) {
-            var boxes = $(this).find('table');
-            var title= $(this).children().eq(0).text().trim();
-            card[title]={};
-            return boxes.each (function() {
-                var rows =  $(this).children().eq(0).children();
-                var sub_title = rows.eq(0).children().eq(0).children().eq(1).text().trim();
-                var card_text = rows.eq(2).text().trim();
-                card[title][sub_title] = card_text;
-            });
-        });
-/*         $(".cardtable-cardimage").each( function(index) {
-            return card.picture = $(this);
-            }); */
+        desc = $(".navbox-list").eq(0).html().replace('<br>','\n').replace(/<.*?>/g,'').replace(/&apos;/g, "'").replace(/&quot;/g, '"').replace(/&gt;/g, '>').replace(/&lt;/g, '<').replace(/&amp;/g, '&').trim();
+        picture = $(".cardtable-cardimage").eq(0).html().match(/<a href=\"(.*?)\"/)[1];
         card_json = {"ocg":{}, "tcg":{}};
         /*card_json.ocg.pack = 
         card_json.ocg.pack_id = 
@@ -30,7 +24,7 @@ scraperjs.StaticScraper.create()
         card_json.tcg.pack_id =
         card_json.tcg.date =*/
         card_json.id = parseInt(card.Passcode);
-        //card_json.setcode =
+        card_json.setcode = setcode;
         card_json.type = (card.Types.split(' / ')[1] + ' / ' + card.Types.split(' / ')[2] + ' / ' + card.Types.split(' / ')[3]).replace('/ undefined','').replace('  / undefined','').trim();
         card_json.atk = parseInt(card['ATK / DEF'].split(' / ')[0]);
         card_json.def = parseInt(card['ATK / DEF'].split(' / ')[1]);
@@ -38,8 +32,7 @@ scraperjs.StaticScraper.create()
         card_json.race = card.Types.split(' / ')[0].trim();
         card_json.attribute = card.Attribute.trim();
         card_json.name = card.English.trim().replace('Check translation','');
-        card_json.desc = card['Card descriptions'].English;
-        //card_json.picture = card.picture;
-        console.log(card);
-        //console.log(card_json);
+        card_json.desc = desc;
+        card_json.picture = picture;
+        console.log(card_json);
     });
